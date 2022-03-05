e703e08402ff9fb92c7ede6853768e55f2a4dcbb
saradelrio131 <saradelriogarcia@gmail.com>
2022-03-05 20:58:56 +0000

    Add 03-playbook.yaml

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -2,7 +2,14 @@
 
 #Commands to run
 
+#Perform initialization taks on all VMs
 ansible-playbook -i hosts 01-playbook.yaml
-ansible-playbook -i hosts -l master 02-playbook.yaml
-ansible-playbook -i hosts -l workers 03-playbook.yaml
-ansible-playbook -i hosts -l nfs 04-playbook.yaml
+
+#Install NFS server
+ansible-playbook -i hosts 02-playbook.yaml
+
+#Perform common tasks on all k8s nodes
+ansible-playbook -i hosts 03-playbook.yaml
+
+#
+ansible-playbook -i hosts 04-playbook.yaml
