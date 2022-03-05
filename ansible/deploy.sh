ae3528ab557df7086490eff225ef4dbd8dea12e5
Sara del Rio <srio@sciops.esa.int>
2022-03-05 13:41:53 +0100

    Update deploy.sh

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -2,7 +2,7 @@
 
 #Commands to run
 
-ansible-playbook -h hosts 01-playbook.yaml
-ansible-playbook -h hosts -l master 02-playbook.yaml
-ansible-playbook -h hosts -l workers 03-playbook.yaml
-ansible-playbook -h hosts -l nfs 04-playbook.yaml
+ansible-playbook -i hosts 01-playbook.yaml
+ansible-playbook -i hosts -l master 02-playbook.yaml
+ansible-playbook -i hosts -l workers 03-playbook.yaml
+ansible-playbook -i hosts -l nfs 04-playbook.yaml
