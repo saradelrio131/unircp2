04f099a12a7994376317c0be30c2523f921fcdad
saradelrio131 <saradelriogarcia@gmail.com>
2022-03-05 22:44:00 +0000

    Remove configure & install roles

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#Commands to run
+# Commands to run
 
-#Perform initialization taks on all VMs
+# Perform initialization taks on all VMs
 ansible-playbook -i hosts 01-playbook.yaml
 
-#Install NFS server
+# Install NFS server
 ansible-playbook -i hosts 02-playbook.yaml
 
-#Perform common tasks on all k8s nodes
+# Perform common tasks on all k8s nodes
 ansible-playbook -i hosts 03-playbook.yaml
 
-#
+# Configure k8s on master node 
 ansible-playbook -i hosts 04-playbook.yaml
